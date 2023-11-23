929cdddeb568ed618e70bfa3a4122191e1de7e3e
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-11-23 09:37:29 +0100

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Welcome from './components/Welcome'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
+import Screen from './components/Screen';
+
 
 function App() {
 
   return (
-    <>
-     <Welcome/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/screen" element={<Screen />} />
+      </Routes>
+    </Router>
   )
 }
 
