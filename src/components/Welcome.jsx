d14d876ec2259d2ce692cd2c7c896c5fc9c9fb28
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-11-23 09:38:04 +0100

    Updated routing

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Image1 from '../assets/image 1.png'
 import Logo from '/chatdb.png'
 
@@ -29,13 +30,13 @@ const Welcome = () => {
                   No more navigating through complicated menus or
                   forms simply chat with our ChatDB to find what you&lsquore looking for
                 </p>
-
-                <a
+                
+                <Link
                   className="mb-2 inline-block rounded-lg bg-[#40B6AE] px-20 pt-4 pb-4 text-sm font-medium uppercase leading-normal text-white  transition duration-150 ease-in-out hover:bg-[#2d8f89] focus:bg-primary-600  active:bg-primary-700  dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]  md:mr-2 md:mb-0"
-                  href="#"
+                  to="/screen"
                 >
                   Message ChatDB
-                </a>
+                </Link>
 
               </div>
               <div className="mb-12 lg:mb-0">
