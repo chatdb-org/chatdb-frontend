cc3201b5b5bcbd793a34611b61dba3ba6fda4bb8
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-11-27 16:17:57 +0300

    added second screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Welcome from './components/Welcome'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Screen from './components/Screen';
+import Screen2 from './components/Screen2';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/screen" element={<Screen />} />
+        <Route path="/screen2" element={<Screen2 />} />
       </Routes>
     </Router>
   )
